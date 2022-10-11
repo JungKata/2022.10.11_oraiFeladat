653e1710e43270e651db610016c42a0b0cfe8286
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-11 12:13:23 +0200

    3.feladat

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('gomb').addEventListener('click', () => {
         console.log(tombkiir(randomTomb));
     });
+    document.getElementById('Hozzaad').addEventListener('click', () => {
+        let bemenoSzam = parseInt(document.getElementById('bekertSzam').value)
+        tomb.push(bemenoSzam);
+        console.log(tomb);
+    })
    
 });
 //console.log(tombkiir(randomTomb));
