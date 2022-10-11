14590d0b63d3e210d52aae1295b3c12d54a403e4
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-11 11:55:14 +0200

    elso feladat

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
-let t = [];
+const randomTomb = Array(20).fill().map(() => Math.floor(30 * Math.random() + 1));
+let tomb = [];
+function tombkiir(array){
+tomb.push(array.filter(array => array % 5 == 0))
+return tomb;
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-})
+}
+console.log(tombkiir(randomTomb));
+//document.addEventListener('DOMContentLoaded', () => {
+  //  console.log(t);
+//})
